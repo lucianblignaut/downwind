0255000a36fbe8872d20e1568fbc9e751fa7714c
lucian_blignaut <lucian.blignaut@aspiresoftware.com>
2022-07-24 11:07:29 +0200

    improved styling of mobile nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,17 +7,24 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  //will determine whether the viewport is for a handset or not
   isHandset = false
+  //will be used to set classes depending on whether the sidenav is opened or closed
   sidenavOpened = false
+  //will emit true if sidenav should be opened and false if it should be closed
   @Output() openSidenav = new EventEmitter<boolean>()
+  //will remove the header shadow if the sidenav is opened
+  @Input() hideShadow: boolean = false
   constructor(private responsive: BreakpointObserver) { }
 
   ngOnInit(): void {
+    //listen for the viewport becoming smaller than 1194px
     this.responsive.observe(['(max-width: 1194px)']).subscribe((result: BreakpointState) => {
       if (result.matches) {
         this.isHandset = true
       }
     })
+    //listen for the viewport becoming larger than 1194px
     this.responsive.observe(['(min-width: 1194px)']).subscribe((result: BreakpointState) => {
       if (result.matches) {
         this.isHandset = false
